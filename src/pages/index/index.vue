8013a43b94733ccda591a09e3474db795f900761
百溪 <2963637346@qq.com>
2025-03-05 20:35:40 +0800

    轮播图-通用组件导入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 //
+// import XtxSwiper from '@/components/XtxSwiper.vue';
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue';
 </script>
 
 <template>
   <CustomNavbar />
+  <XtxSwiper />
 <div>123</div>
 </template>
 <style lang="scss">
