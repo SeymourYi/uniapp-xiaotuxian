1fc74a7255c1f1e4fcbad8b9fdc3af18ecf0e758
百溪 <2963637346@qq.com>
2025-03-06 10:03:31 +0800

    推荐模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
 //
 // import XtxSwiper from '@/components/XtxSwiper.vue';
-import CustomNavbar from '@/pages/index/components/CustomNavbar.vue';
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home';
-import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
-import CategoryPanel from './components/CategoryPanel.vue';
-const BannerList=ref([])
-const CategoryList=ref([])
-const getHomeBannerData= async()=>{
- const res:any =await  getHomeBannerAPI()
- BannerList.value=res.data.result,
- console.log(BannerList.value);
+import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import HotPanel from './components/HotPanel.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+const BannerList = ref([])
+const CategoryList = ref([])
+const HotList = ref([])
+const getHomeBannerData = async () => {
+  const res: any = await getHomeBannerAPI()
+  BannerList.value = res.data.result
 }
-const getHomeCategoryData=async()=>{
- const res:any=await getHomeCategoryAPI()
- CategoryList.value=res.data.result
-  console.log('====================================');
-  console.log(CategoryList.value,"123");
-  console.log('====================================');
+const getHomeCategoryData = async () => {
+  const res: any = await getHomeCategoryAPI()
+  CategoryList.value = res.data.result
 }
-onLoad(()=>{
- getHomeBannerData()
- getHomeCategoryData()
+
+const getHomeHotData = async () => {
+  const res: any = await getHomeHotAPI()
+  HotList.value = res.data.result
+  console.log(HotList.value)
+}
+onLoad(() => {
+  getHomeBannerData()
+  getHomeCategoryData()
+  getHomeHotData()
 })
 </script>
 
@@ -30,7 +35,8 @@ onLoad(()=>{
   <CustomNavbar />
   <XtxSwiper :List="BannerList" />
   <CategoryPanel :list="CategoryList" />
-<div>123</div>
+  <HotPanel :list="HotList" />
+  <div>123</div>
 </template>
 <style lang="scss">
 //
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,18 +1,25 @@
-import { http } from "@/utils/http"
+import { http } from '@/utils/http'
 
-export const getHomeBannerAPI = (distributionSite=1)=>{
-return http({
-  method:'GET',
-  url:'/home/banner',
-  data:{
-    distributionSite,
-  }
-})
+export const getHomeBannerAPI = (distributionSite = 1) => {
+  return http({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite,
+    },
+  })
 }
 
-export const getHomeCategoryAPI=()=>{
-return http({
-  method:"GET",
-  url:'/home/category/mutli'
-})
+export const getHomeCategoryAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/home/category/mutli',
+  })
+}
+
+export const getHomeHotAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/home/hot/mutli',
+  })
 }
